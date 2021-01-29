e27a9059a622f3ee4588b48998872e447a8a51d3
Shogo Ohta <athos0220@gmail.com>
2021-01-30 00:05:17 +0900

    Update type-infer

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/athos/sweet-array"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[type-infer "0.1.0-SNAPSHOT"]]
+  :dependencies [[type-infer "0.1.0"]]
   :repl-options {:init-ns sweet-array.core}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.2"]]}})
